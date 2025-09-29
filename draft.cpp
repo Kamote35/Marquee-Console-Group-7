8c13247dd44ce6de7040ecddf9dc8814dc88e033
gerald_corpuz@dlsu.edu.ph <gerald_corpuz@dlsu.edu.ph>
2025-09-29 08:31:24 +0800

    How is our OS emulator working properly without display handler lmao, ayusin nalang mamaya wtf

--- draft.cpp
+++ draft.cpp
@@ -17,14 +17,38 @@
 // global
 std::string marqueeText = "Welcome to the C++ Marquee Emulator!";
 std::atomic<bool> marqueeRunning(false); // thread-safe boolean flag
-std::atomic<int> marqueeSpeed(200); // thread-safe int variable, sets speed of marquee display
-// in miliseconds
+std::atomic<int> marqueeSpeed(200); // thread-safe int variable, sets speed of marquee in miliseconds
 std::mutex textMutex;
 std::queue<std::string> keyboard_queue;
 std::mutex queue_mutex;
 std::atomic<bool> keyboard_stop(false);
+std::atomic<bool> command_stop(false);
+std::atomic<bool> marquee_stop(false);
 
 
+// utility: move cursor to (x,y)
+void setCursorPosition(int x, int y) {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(hOut, pos);
+}
+
+// get console size
+int getConsoleWidth() {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+}
+
+// clear an entire line y
+void clearLine(int y) {
+    int width = getConsoleWidth();
+    setCursorPosition(0, y);
+    std::cout << std::string(width, ' ') << std::flush;
+    setCursorPosition(0, y);
+}
+
+// display starting message
 void startMessage() {
     using namespace std; // do not declare this global, since we have multiple libraries 
 	cout << "WELCOME to CSOPESY!\n";
@@ -35,73 +59,130 @@ void startMessage() {
 	cout << "Manaois, Chriscel John\n";
 	cout << "Sayat, John Christian\n";
 	
-	cout << "Version date: 2.0\n";
+	cout << "Version date: 3.3\n\n";
 }
 
-void commandInterpreter() {
-    
-    std::string command;
-    while (true) {
-        std::cout << "\nCommand> ";
-        std::cin >> command;
+// Add this function near your other utility functions
+void displayPrompt() {
+    std::cout << "Command>" << std::flush;
+}
 
-        if (command == "help") {
-            std::cout << "Available commands:\n"
-                 << "  \"help\"          - Display the commands and their use.  \n"
-                 << "  \"start_marquee\" - Start the marquee \"animation\".     \n"
-                 << "  \"stop_marquee\"  - Stop the marquee \"animation\".      \n"
-                 << "  \"set_text\"      - Change the marquee text.             \n"
-                 << "  \"set_speed\"     - Change marquee speed (ms).           \n"
-                 << "  \"exit\"          - Quit program.                        \n";
+// handles OS emulator with marquee animation commands
+void commandInterpreter() {
+    while (!command_stop) {
+        if (!keyboard_queue.empty()) {
+            std::string command;
+            {
+                std::lock_guard<std::mutex> lock(queue_mutex);
+                if (!keyboard_queue.empty()) {
+                    command = keyboard_queue.front();
+                    keyboard_queue.pop();
+                } else {
+                    continue;
+                }
+            }
+
+            // Handle the command first
+            if (command == "help") {
+                std::cout << "Available commands:\n"
+                     << "  \"help\"          - Display the commands and their use.  \n"
+                     << "  \"start_marquee\" - Start the marquee \"animation\".     \n"
+                     << "  \"stop_marquee\"  - Stop the marquee \"animation\".      \n"
+                     << "  \"set_text\"      - Change the marquee text.             \n"
+                     << "  \"set_speed\"     - Change marquee speed (ms).           \n"
+                     << "  \"exit\"          - Quit program.                        \n"
+                     << std::endl;
+                displayPrompt();  // Display prompt after command output
+            }
+            else if (command == "start_marquee") {
+                if (!marqueeRunning) {
+                    marqueeRunning = true;
+                }
+                else std::cout << "Marquee animation is already running!\n";
+                std::cout << std::endl;
+				displayPrompt();
+            }
+            else if (command == "stop_marquee") {
+                if (marqueeRunning) {
+                    marqueeRunning = false;
+                }
+                else std::cout << "Marquee animation is not running!\n";
+                std::cout << std::endl;
+                displayPrompt();
+            }
+            else if (command == "set_text") {
+                std::cout << "Enter new text: ";
+                while (keyboard_queue.empty()) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                }
+                std::string newText;
+                {
+                    std::lock_guard<std::mutex> lock(queue_mutex);
+                    newText = keyboard_queue.front();
+                    keyboard_queue.pop();
+                }
+                {
+                    std::lock_guard<std::mutex> lock(textMutex);
+                    marqueeText = newText;
+                }
+                std::cout << std::endl;
+                displayPrompt();
+            }
+            else if (command == "set_speed") {
+                std::cout << "Enter speed in ms: ";
+                while (keyboard_queue.empty()) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                }
+                std::string speedStr;
+                {
+                    std::lock_guard<std::mutex> lock(queue_mutex);
+                    speedStr = keyboard_queue.front();
+                    keyboard_queue.pop();
+                }
+                try {
+                    int speed = std::stoi(speedStr);
+                    if (speed > 0) marqueeSpeed = speed;
+                } catch (...) {
+                    std::cout << "Invalid speed value\n";
+                }
+                std::cout << std::endl;
+                displayPrompt();
+            }
+            else if (command == "exit") {
+                std::cout << "Exiting console...\n";
+                command_stop = true;
+                keyboard_stop = true;
+                marquee_stop = true;  // Add this line
+                break;
+            }
+            else {
+                std::cout << "Unknown command. Type 'help' for options.\n\n";
+                displayPrompt();
+            }
         }
-        else if (command == "start_marquee") {
-            marqueeRunning = true;
-        }
-        else if (command == "stop_marquee") {
-            marqueeRunning = false;
-        }
-        else if (command == "set_text") {
-            using namespace std;
-            cout << "Enter new text: ";
-            cin.ignore(); 
-            string newText;
-            getline(cin, newText);
-            lock_guard<mutex> lock(textMutex);
-            marqueeText = newText;
-        }
-        else if (command == "set_speed") {
-            using namespace std;
-            cout << "Enter speed in ms: ";
-            int speed;
-            cin >> speed;
-            if (speed > 0) marqueeSpeed = speed;
-        }
-        else if (command == "exit") {
-            std::cout << "Exiting console...\n";
-            exit(0);
-        }
-        else {
-            std::cout << "Unknown command. Type 'help' for options.\n";
-        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
 
-// Handles the Keyboard inputs
+// handles the Keyboard inputs
 void keyboardHandler() { 
     std::string input_buffer = "";
+
+    // for debugging
+    //std::cout << "Keyboard Handler Debug\n";
 
     while (!keyboard_stop) {
         if (_kbhit()) { // check any input from the keyboard
             char key = _getch(); // get pressed key
 
             // Check for ENTER key input (Priority)
-            if (key == '\r') { // use carriage return '\r' to detect an Enter key input. 
-                               // '\n' is not actual keyboard input, remember CSARCH2 lol.
-
-                std::lock_guard<std::mutex> lock(queue_mutex); // Lock the queue while pushing, automatically unlocks after
+            if (key == '\r') {
+                std::lock_guard<std::mutex> lock(queue_mutex);
                 keyboard_queue.push(input_buffer);
-                input_buffer = ""; // reinitialize 
-                std::cout << std::endl; // automatically newline after Enter
+                input_buffer = "";
+                std::cout << std::endl;
+                
+                
             }
             // Check for Backspace
             else if (key == '\b') {
@@ -123,27 +204,79 @@ void keyboardHandler() {
 
 
         // sleep this thread's while loop, give chance for marquee animation
-        std::this_thread::sleep_for(std::chrono::milliseconds(1)); 
+        std::this_thread::sleep_for(std::chrono::microseconds(500)); 
        
 
     }
 
 }
 
+void marqueeWorker() {
+    size_t pos = 0;
+    std::string text;
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    while (!marquee_stop) {  // Check stop condition
+        if (marqueeRunning) {
+            {
+                std::lock_guard<std::mutex> lock(textMutex);
+                text = marqueeText + "   ";
+            }
+
+            int width = getConsoleWidth();
+            std::string display(width, ' ');
+
+            for (size_t i = 0; i < text.size(); i++) {
+                int index = (pos + i) % text.size();
+                if (i < (size_t)width) display[i] = text[index];
+            }
+
+            // Save cursor position (so typing isn’t disturbed)
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+            GetConsoleScreenBufferInfo(hOut, &csbi);
+            COORD saved = csbi.dwCursorPosition;
+
+            // Write marquee at line 0
+            setCursorPosition(0, 0);
+            std::cout << display << std::flush;
+
+            // Restore cursor position for user typing
+            SetConsoleCursorPosition(hOut, saved);
+
+            pos = (pos + 1) % text.size();
+            std::this_thread::sleep_for(std::chrono::milliseconds(marqueeSpeed));
+        }
+        else {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+}
+
+
 
 int main() {
-	
-    std::thread kb_thread(keyboardHandler); // Keyboard Handler thread
+    // Thread Creation
+    std::thread kb_thread(keyboardHandler); 
+    std::thread marqueeThread(marqueeWorker); 
+    std::thread cmd_thread(commandInterpreter);
 
-    // main thread
-	startMessage();
-	commandInterpreter();
+    // Main thread
+    startMessage();
+    displayPrompt();  // Initial prompt
 
-
-    keyboard_stop = true; // end keyboard thread constant checking for inputs
+	// Sleep Main thread
+    while (!command_stop) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // give chance to others
+    }
+    
+    // Set all stop flags
+    keyboard_stop = true;
+    marquee_stop = true;  // Add this line
     
     // CLEANUP
+    marqueeThread.join();
     kb_thread.join();
-	
-	return 0;
+    cmd_thread.join();
+    
+    return 0;
 }
